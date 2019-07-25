7dc2455cfd533517e03488bb6a428998555f1a1f
Leonid Ivanov <leonid_ivanov@epam.com>
2019-07-25 15:10:58 +0300

    initial commit

---
+++ backend/src/main/java/com/epam/webchat/leonidivanov/configuration/WebChatConfiguration.java
@@ -0,0 +1,12 @@
+package com.epam.webchat.leonidivanov.configuration;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebChatConfiguration {
+
+
+}
