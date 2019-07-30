fcb4733fef5d204099cea13722413ba97a98a8ee
Leonid Ivanov <leonid_ivanov@epam.com>
2019-07-30 17:14:09 +0300

    Refactored auth

--- backend/src/main/java/com/epam/webchat/leonidivanov/configuration/SecurityConfig.java
+++ backend/src/main/java/com/epam/webchat/leonidivanov/configuration/SecurityConfig.java
@@ -15,8 +15,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    private static final String ADMIN_ENDPOINT = "/api/v1/admin/**";
-    private static final String LOGIN_ENDPOINT = "/api/v1/auth/login";
+    private static final String ADMIN_ENDPOINT = "/admin/**";
+    private static final String LOGIN_ENDPOINT = "/auth/login";
 
     @Autowired
     public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
